ab99e5816b19bf0cb813d88551c3f6b937b24125
Marius Grigoriu <marius.grigoriu@live.com>
2015-07-19 13:51:09 -0700

    refactor

--- daily_trello.go
+++ daily_trello.go
@@ -15,6 +15,14 @@ type List struct {
 }
 
 func main(){	
+	lists := GetTrelloLists("534efa63a3a33edc034ac3d1")
+	
+	for _, list := range lists {
+		fmt.Printf("%s: %d\n", list.Name, len(list.Cards))
+	}	
+}
+
+func GetTrelloLists(boardID string) (lists []List) {
 	query := url.Values{
 		"key": {key}, 
 		"token": {token},
@@ -24,7 +32,7 @@ func main(){
 	boardUrl := url.URL{
 		Scheme: "https",
 		Host: "api.trello.com",
-		Path: "1/board/534efa63a3a33edc034ac3d1/lists",
+		Path: "1/board/" + boardID + "/lists",
 		RawQuery: query.Encode(),
 	}
 	response, err := http.Get(boardUrl.String())
@@ -34,13 +42,9 @@ func main(){
 	defer response.Body.Close()
 	
 	decoder := json.NewDecoder(response.Body)
-	var lists []List
 	err = decoder.Decode(&lists)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	for _, list := range lists {
-		fmt.Printf("%s: %d\n", list.Name, len(list.Cards))
-	}	
+	return
 }
