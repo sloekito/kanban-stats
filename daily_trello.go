fdea60bbaa882f7f849b5c6d8eafb310a2fecf89
Marius Grigoriu <marius@Mariuss-MacBook-Air.local>
2015-07-20 16:32:38 -0700

    generate influx json

--- daily_trello.go
+++ daily_trello.go
@@ -14,12 +14,16 @@ type List struct {
 	Cards []interface{}
 }
 
-func main(){	
+func main(){
+	log.Print("---daily_trello: start")	
 	lists := GetTrelloLists("534efa63a3a33edc034ac3d1")
+	PublishListsToInflux(lists)
 	
 	for _, list := range lists {
 		fmt.Printf("%s: %d\n", list.Name, len(list.Cards))
-	}	
+	}
+	
+	log.Print("---daily_trello: success")	
 }
 
 func GetTrelloLists(boardID string) (lists []List) {
@@ -47,4 +51,42 @@ func GetTrelloLists(boardID string) (lists []List) {
 		log.Fatal(err)
 	}
 	return
+}
+
+const (
+	TrelloBoardName = "DevProcess"	
+)
+
+const (
+	InfluxHost = "192.168.99.100"
+	InfluxPort = 8086
+	InfluxDB = "Trello"
+)
+
+type InfluxPost struct {
+	Name string `json:"name"`
+	Columns []string `json:"columns"`
+	Points [1][]int `json:"points"`
+}
+
+func PublishListsToInflux(lists []List){
+	post := InfluxPost{
+		Name: TrelloBoardName,
+	}
+	columns := make([]string, len(lists))
+	point := make([]int, len(lists))
+	points := [1][]int{point}
+	for _, list := range lists {
+		columns = append(columns, list.IdBoard + "_count_open_cards")
+		points[0] = append(points[0], len(list.Cards))
+	}
+	
+	post.Columns = columns
+	post.Points = points
+	
+	body, err := json.Marshal(post)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
 }
