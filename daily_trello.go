7d254852ed958206650907999b9a454ce515c079
Marius Grigoriu <marius@Mariuss-MacBook-Air.local>
2015-07-20 17:02:43 -0700

    post json to influx

--- daily_trello.go
+++ daily_trello.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 	"encoding/json"
+	"bytes"
 )
 
 type List struct {
@@ -58,9 +59,10 @@ const (
 )
 
 const (
-	InfluxHost = "192.168.99.100"
-	InfluxPort = 8086
+	InfluxHost = "192.168.99.100:8086"
 	InfluxDB = "Trello"
+	InfluxUser = "root"
+	InfluxPass = "root"
 )
 
 type InfluxPost struct {
@@ -88,5 +90,22 @@ func PublishListsToInflux(lists []List){
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(body))
+	
+	query := url.Values{
+		"u": {InfluxUser}, 
+		"p": {InfluxPass},
+	}
+	influxURL := url.URL{
+		Scheme: "http",
+		Host: InfluxHost,
+		Path: "db/" + InfluxDB + "/series",
+		RawQuery: query.Encode(),
+	}
+	response, err := http.Post(influxURL.String(), "application/json", bytes.NewReader(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if response.StatusCode != 200 {
+		log.Fatal("InfluxDB response: " + response.Status)
+	}
 }
