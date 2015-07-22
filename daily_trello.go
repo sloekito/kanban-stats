f797e57723ce72bfb09320f9af7f75c887bc6511
Marius Grigoriu <marius.grigoriu@me.com>
2015-07-22 12:38:29 -0700

    use board id as series name

--- daily_trello.go
+++ daily_trello.go
@@ -19,7 +19,7 @@ func main(){
 		fmt.Printf("%s(%s): %d\n", list.Name, list.Id, len(list.Cards))
 	}
 
-	PublishListsToInflux("DevProcess", lists)
+	PublishListsToInflux("534efa63a3a33edc034ac3d1", lists)
 	
 	
 	log.Print("---daily_trello: success")	
