e55445611f018ac49d7d0162f4ef85f0744d9501
Marius Grigoriu <marius.grigoriu@me.com>
2015-07-23 15:59:32 -0700

    parameterize trello

--- daily_trello.go
+++ daily_trello.go
@@ -3,23 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"flag"
 )
 
 
 func main(){
 	log.Print("---daily_trello: start")
 	
+	trelloKey := flag.String("trellokey", "", "Trello application key")
+	trelloToken :=flag.String("trellotoken", "", "Trello access token")
+	trelloBoardID := flag.String("boardid", "", "Trello board ID")
+	flag.Parse()
+	
 	client := Client{
-		Key: KEY,
-		Token: TOKEN,
-	}	
-	lists := client.GetTrelloLists("534efa63a3a33edc034ac3d1")
+		Key: *trelloKey,
+		Token: *trelloToken,
+	}
+	fmt.Println(client)	
+	lists := client.GetTrelloLists(*trelloBoardID)
 	
 	for _, list := range lists {
 		fmt.Printf("%s(%s): %d\n", list.Name, list.Id, len(list.Cards))
 	}
 
-	PublishListsToInflux("534efa63a3a33edc034ac3d1", lists)
+	PublishListsToInflux(*trelloBoardID, lists)
 	
 	
 	log.Print("---daily_trello: success")	
