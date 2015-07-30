9360261046912dad8e45dba50c4e64650bf52a9a
Marius Grigoriu <marius.grigoriu@me.commit>
2015-07-30 01:28:27 -0700

    parseArgs function

--- daily_trello.go
+++ daily_trello.go
@@ -8,46 +8,48 @@ import (
 	influxdb "daily_trello/internal/github.com/influxdb/influxdb/client"
 )
 
-/*type args struct {
-	trelloKey, trelloToken, trelloBoardID, influxHost, influxDB, influxUser, influxPassword string
-	printOnly bool
+type args struct {
+	trelloKey, trelloToken, trelloBoardID, influxHost, influxDB, influxUser, influxPassword *string
+	printOnly *bool
 }
 
-func parseArgs() args {
-	var _args args
-	
-}*/
+func parseArgs() (_args args) {
+	_args = args{
+		trelloKey: flag.String("trellokey", "", "Trello application key"),
+		trelloToken: flag.String("trellotoken", "", "Trello access token"),
+		trelloBoardID: flag.String("boardid", "", "Trello board ID"),
+		influxHost: flag.String("influxhost", "", "Influx host:post"),
+		influxDB: flag.String("influxdb", "", "Influx datbase name"),
+		influxUser: flag.String("influxuser", "", "Influx username"),
+		influxPassword: flag.String("influxpass", "", "Influx password"),
+		printOnly: flag.Bool("print-only", false, "Print information rather than publish to Influx"),
+	}
+	flag.Parse()
+	return
+}
 
 func main() {
 	log.Print("daily_trello: start")
-
-	trelloKey := flag.String("trellokey", "", "Trello application key")
-	trelloToken := flag.String("trellotoken", "", "Trello access token")
-	trelloBoardID := flag.String("boardid", "", "Trello board ID")
-	influxHost := flag.String("influxhost", "", "Influx host:post")
-	influxDB := flag.String("influxdb", "", "Influx datbase name")
-	influxUser := flag.String("influxuser", "", "Influx username")
-	influxPassword := flag.String("influxpass", "", "Influx password")
-	printOnly := flag.Bool("print-only", false, "Print information rather than publish to Influx")
-	flag.Parse()
+	
+	config := parseArgs()
 
 	trello := trello.Client{
-		Key:   *trelloKey,
-		Token: *trelloToken,
+		Key:   *config.trelloKey,
+		Token: *config.trelloToken,
 	}
-	lists := trello.GetLists(*trelloBoardID)
+	lists := trello.GetLists(*config.trelloBoardID)
 
-	if *printOnly == true {
+	if *config.printOnly == true {
 		for _, list := range lists {
 			fmt.Printf("%s(%s): %d\n", list.Name, list.Id, len(list.Cards))
 		}
 		
 	} else {
 		influxdbClient, err := influxdb.NewClient(&influxdb.ClientConfig{
-			Host: *influxHost,
-			Username: *influxUser,
-			Password: *influxPassword,
-			Database: *influxDB,
+			Host: *config.influxHost,
+			Username: *config.influxUser,
+			Password: *config.influxPassword,
+			Database: *config.influxDB,
 		})
 		if err != nil {
 			log.Fatal(err)
@@ -56,14 +58,14 @@ func main() {
 		databases, err := influxdbClient.GetDatabaseList()
 		databaseFound := false
 		for _,db := range databases {
-			if db["name"] == *influxDB {
+			if db["name"] == *config.influxDB {
 				databaseFound = true
 				break
 			}
 		}
 		
 		if !databaseFound {
-			err = influxdbClient.CreateDatabase(*influxDB)
+			err = influxdbClient.CreateDatabase(*config.influxDB)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -71,7 +73,7 @@ func main() {
 		
 		point := make([]interface{}, len(lists))
 		series := influxdb.Series{	
-			Name: *trelloBoardID,
+			Name: *config.trelloBoardID,
 			Columns: make([]string, len(lists)),
 			Points: [][]interface{}{point},
 		}
