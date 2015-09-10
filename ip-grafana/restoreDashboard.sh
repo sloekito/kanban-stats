b4b5f5bdb23e70d50b96472eb11cd106e132bde3
Marius Grigoriu <marius.grigoriu@me.commit>
2015-09-10 11:03:09 -0700

    grafana dashboard config

---
+++ ip-grafana/restoreDashboard.sh
@@ -0,0 +1,2 @@
+curl -u admin:Innov@tionL@b -H 'Content-Type: application/json' -X POST -d @trello.json http://grafana.cloud.nlab.io/api/datasources
+curl -u admin:Innov@tionL@b -H 'Content-Type: application/json' -X POST -d @teamStats.json http://grafana.cloud.nlab.io/api/dashboards/db
