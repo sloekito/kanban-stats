f0ad720f9e0c5afad1e1fdcc4edf89003e917c34
Marius Grigoriu <marius.grigoriu@me.commit>
2015-09-27 12:35:01 -0700

    move database code to separate file

--- main.go
+++ main.go
@@ -60,21 +60,3 @@ func main() {
 		writeListsToDatabase(influxdbClient, lists, flags.trelloBoardID)
 	}
 }
-
-func writeListsToDatabase(client *influxdb.Client, lists []trello.List, seriesName string){
-	point := make([]interface{}, len(lists))
-	series := influxdb.Series{	
-		Name: seriesName,
-		Columns: make([]string, len(lists)),
-		Points: [][]interface{}{point},
-	}
-	for i, list := range lists {
-		series.Columns[i] = list.Id + "_count_open_cards"
-		point[i] = len(list.Cards)
-	}
-	
-	err := client.WriteSeries([]*influxdb.Series{&series})
-	if err != nil {
-		log.Fatal(err)
-	}
-}
