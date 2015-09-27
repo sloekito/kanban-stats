2f19d4339689fdf2e36266a597e4704416bab0e5
Marius Grigoriu <marius.grigoriu@me.commit>
2015-09-27 12:00:04 -0700

    cleaner handling of flags

--- main.go
+++ main.go
@@ -8,23 +8,21 @@ import (
 	influxdb "trello-stats/internal/github.com/influxdb/influxdb/client"
 )
 
-type args struct {
-	trelloKey, trelloToken, trelloBoardID, influxHost, influxDB, influxUser, influxPassword *string
-	verbose, dryRun *bool
+type flags struct {
+	trelloKey, trelloToken, trelloBoardID, influxHost, influxDB, influxUser, influxPassword string
+	verbose, dryRun bool
 }
 
-func parseArgs() (_args args) {
-	_args = args{
-		trelloKey: flag.String("trellokey", "", "Trello application key"),
-		trelloToken: flag.String("trellotoken", "", "Trello access token"),
-		trelloBoardID: flag.String("boardid", "", "Trello board ID"),
-		influxHost: flag.String("influxhost", "", "Influx host:post"),
-		influxDB: flag.String("influxdb", "", "Influx datbase name"),
-		influxUser: flag.String("influxuser", "", "Influx username"),
-		influxPassword: flag.String("influxpass", "", "Influx password"),
-		verbose: flag.Bool("v", false, "Print verbose information"),
-		dryRun: flag.Bool("d", false, "Dry run does not output to database"),
-	}
+func getCommandLineFlags() (flags flags) {
+	flag.StringVar(&flags.trelloKey, "trellokey", "", "Trello application key")
+	flag.StringVar(&flags.trelloToken, "trellotoken", "", "Trello access token")
+	flag.StringVar(&flags.trelloBoardID, "boardid", "", "Trello board ID")
+	flag.StringVar(&flags.influxHost, "influxhost", "", "Influx host:post")
+	flag.StringVar(&flags.influxDB, "influxdb", "", "Influx datbase name")
+	flag.StringVar(&flags.influxUser, "influxuser", "", "Influx username")
+	flag.StringVar(&flags.influxPassword, "influxpass", "", "Influx password")
+	flag.BoolVar(&flags.verbose, "v", false, "Print verbose information")
+	flag.BoolVar(&flags.dryRun, "d", false, "Dry run does not output to database")
 	flag.Parse()
 	return
 }
@@ -33,33 +31,33 @@ func main() {
 	log.Print(ApplicationName, ": start")
 	defer log.Print(ApplicationName, ": end")
 	
-	config := parseArgs()
+	flags := getCommandLineFlags()
 
 	trello := trello.Client{
-		Key:   *config.trelloKey,
-		Token: *config.trelloToken,
+		Key:   flags.trelloKey,
+		Token: flags.trelloToken,
 	}
-	lists := trello.GetLists(*config.trelloBoardID)
+	lists := trello.GetLists(flags.trelloBoardID)
 
-	if *config.verbose == true {
-		fmt.Printf("Board ID: %v\n", *config.trelloBoardID)
+	if flags.verbose == true {
+		fmt.Printf("Board ID: %v\n", flags.trelloBoardID)
 		for _, list := range lists {
 			fmt.Printf("%s(%s): %d\n", list.Name, list.Id, len(list.Cards))
 		}
 	}
 	
-	if !*config.dryRun {
+	if !flags.dryRun {
 		influxdbClient, err := influxdb.NewClient(&influxdb.ClientConfig{
-			Host: *config.influxHost,
-			Username: *config.influxUser,
-			Password: *config.influxPassword,
-			Database: *config.influxDB,
+			Host: flags.influxHost,
+			Username: flags.influxUser,
+			Password: flags.influxPassword,
+			Database: flags.influxDB,
 		})
 		if err != nil {
 			log.Fatal(err)
 		}
 	
-		writeListsToDatabase(influxdbClient, lists, *config.trelloBoardID)
+		writeListsToDatabase(influxdbClient, lists, flags.trelloBoardID)
 	}
 }
 
